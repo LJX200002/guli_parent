8283d8d7d7082d422dd194729f95ce50e82f03a3
LJX <740954966@qq.com>
2022-04-04 21:58:18 +0800

    first commit

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -115,6 +115,7 @@ public class EduTeacherController {
     @PostMapping("updateTeacher")
     public R updateTeacher(@RequestBody EduTeacher eduTeacher){
         boolean b = eduTeacherService.updateById(eduTeacher);
+        System.out.println("xx");
         if(b==true){
             return R.ok();
         }else{
